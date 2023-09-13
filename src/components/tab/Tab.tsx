0cb09597961394c281ba0c1cd8cc30bac74895a0
ddaeunbb <ddaeunbb@gmail.com>
2023-09-13 16:57:06 +0900

    squash & merge

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -4,12 +4,13 @@ import { ReactNode } from 'react';
 interface TabProp {
 	// title: string;
 	children: ReactNode;
+	path: string;
 }
 
-export default function Tab({ children }: TabProp) {
+export default function Tab({ path, children }: TabProp) {
 	return (
 		<Link
-			href="/"
+			href={path}
 			className="flex items-center px-3 py-1.5 transition-all font-normal rounded-lg hover:bg-[rgba(237,237,237,1)] text-tabGary"
 		>
 			{children}
---
+++ src/router/path.ts
@@ -0,0 +1,9 @@
+const Path = {
+	home: '/',
+	log: '/log',
+	articles: '/articles',
+	archives: '/archives',
+	note: '/note',
+};
+
+export default Path;
