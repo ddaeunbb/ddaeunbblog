bc931a2164e9c2fc978ad1021ba84f446f3aadbb
ddaeunbb <ddaeunbb@gmail.com>
2023-09-13 16:26:38 +0900

    feat: footer - footer 완성하였습니다. - 메인페이지도 대충 레이아웃을 구성하였습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,36 @@
 export default function Home() {
 	return (
-		<div className="font-normal">
-			<h1 className="text-5xl font-extrabold">ddaeunbb</h1>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam
-			atque beatae ea. Obcaecati doloremque inventore voluptates officiis
-			tenetur similique, consectetur, expedita facere consequatur dicta
-			architecto? At dolorum delectus vitae? 가지에 예가 귀는 말이다. 것이
-			노년에게서 그러므로 실현에 때문이다. 인생에 하는 피가 이상이 아니다. 없는
-			영원히 청춘 이상을 인류의 커다란 있으며, 우리 교향악이다. 귀는 원질이 너의
-			그와 내려온 스며들어 사막이다. 따뜻한 물방아 수 사라지지 온갖 대한 미묘한
-			청춘에서만 천하를 때문이다. 이것이야말로 과실이 찾아다녀도, 수 싶이
-			오아이스도 우리 예수는 그들의 운다. 속잎나고, 못하다 관현악이며, 두손을
-			그림자는 새 설레는 없으면 용감하고 것이다. 사라지지 붙잡아 곳이
-			봄바람이다. 미인을 이상의 쓸쓸한 가슴에 튼튼하며, 그러므로 때문이다.
+		<div>
+			<section className="font-normal">
+				<h1 className="mb-4 text-3xl font-extrabold tracking-tight sm:text-5xl">
+					ddaeunbb
+				</h1>
+				<main className="mt-0.5 inline-block font-bold underline-offset-4 hover:underline mb-4">
+					Frontend Developer
+				</main>
+				<div>
+					<p className="text-middleGray mb-4">
+						Junior developer who studies step by step like a chick.
+					</p>
+				</div>
+				<div>
+					<a
+						href="https://ddaeunbb.tistory.com/"
+						target="_blank"
+						className="flex gap-x-1 items-center"
+					>
+						<span className="text-lightGray hover:text-basicFont">
+							More about me ⇢
+						</span>
+					</a>
+				</div>
+			</section>
+
+			<section className="mt-16">
+				<h2 className="text-2xl font-bold tracking-tight md:text-4xl mb-6">
+					Featured Posts
+				</h2>
+			</section>
 		</div>
 	);
 }
---
+++ src/components/icons/ArrowIcon.tsx
@@ -0,0 +1,19 @@
+const ArrowIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={24}
+		height={24}
+		fill="none"
+		viewBox="0 0 24 24"
+		className="stroke-lightGray"
+	>
+		<path
+			stroke="color"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={2}
+			d="M4 12h16m0 0-4-4m4 4-4 4"
+		/>
+	</svg>
+);
+export default ArrowIcon;
