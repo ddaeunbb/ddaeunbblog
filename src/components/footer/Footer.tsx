29d2fa948625b674488e2e081e248b19679a509b
ddaeunbb <ddaeunbb@gmail.com>
2023-09-13 16:00:11 +0900

    feat: footer - footer 레이아웃 완성하였습니다. - 다크모드 구현 추가 수정필요합니다.

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,58 @@
+import GithubIcon from '@/components/icons/GithubIcon';
+import InstaIcon from '@/components/icons/InstaIcon';
+import MailIcon from '@/components/icons/MailIcon';
+
+export default function Footer() {
+	return (
+		<footer className="pb-8 text-sm text-neutral-800 dark:text-neutral-400">
+			<hr className="border-1 w-full border-neutral-300 transition-all dark:border-neutral-700 mb-8" />
+			<div className="flex flex-col items-end space-y-1">
+				<div className="flex space-x-2">
+					<a
+						href="mailto:ddaeunbb@gmail.com"
+						target="_blank"
+						className="transition"
+					>
+						<MailIcon />
+					</a>
+					<a
+						href="https://github.com/ddaeunbb"
+						target="_blank"
+						className="transition"
+					>
+						<GithubIcon />
+					</a>
+					<a
+						href="https://www.instagram.com/daeunx_x/"
+						target="_blank"
+						className="transition"
+					>
+						<InstaIcon />
+					</a>
+				</div>
+				<p>
+					<span>© 2023 </span>
+					<a>ddaeunbblog </a>
+					<span>Powerd by </span>
+					<a
+						href="https://nextjs.org/"
+						className="transition hover:text-secondary"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Next.js
+					</a>
+					<span>, </span>
+					<a
+						href="https://pages.github.com/"
+						className="transition hover:text-secondary"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Github Pages
+					</a>
+				</p>
+			</div>
+		</footer>
+	);
+}
---
+++ src/components/icons/MailIcon.tsx
@@ -0,0 +1,15 @@
+const MailIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xmlSpace="preserve"
+		width={20}
+		height={20}
+		className="hover:fill-middleGray"
+		fill="color"
+		viewBox="0 0 32 32"
+	>
+		<path d="M22 19c-.6 0-1-.4-1-1V1c0-.6.4-1 1-1h8c.6 0 1 .4 1 1v4c0 .6-.4 1-1 1h-7v12c0 .6-.4 1-1 1zM14.9 16.9c0-4-3-7.3-6.9-7.8C4 9.6 1 13 1 17v8c0 .6.4 1 1 1h12.9v-9.1zM12 28v3c0 .6.4 1 1 1h6c.6 0 1-.4 1-1v-3h-8z" />
+		<path d="M25 9.3V18c0 .6-.4 1-1 1s-1-.4-1-1V9H13c2.4 1.8 3.9 4.7 3.9 7.9V26H30c.6 0 1-.4 1-1v-8c0-3.7-2.6-6.8-6-7.7z" />
+	</svg>
+);
+export default MailIcon;
