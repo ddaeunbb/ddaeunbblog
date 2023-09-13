a7f9f2a6481851141a8cf0c69955183f3ec12add
ddaeunbb <ddaeunbb@gmail.com>
2023-09-13 17:32:18 +0900

    feat: blog - 블로그 페이지를 대략 구성하였습니다.

---
+++ src/components/icons/SearchIcon.tsx
@@ -0,0 +1,27 @@
+const SearchIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={30}
+		height={30}
+		fill="none"
+		viewBox="0 -0.5 25 25"
+		className="stroke-middleGray absolute right-3 top-2"
+	>
+		<path
+			stroke="color"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={1.5}
+			d="M5.5 10.765a5.765 5.765 0 1 1 11.53 0 5.765 5.765 0 0 1-11.53 0Z"
+			clipRule="evenodd"
+		/>
+		<path
+			stroke="color"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={1.5}
+			d="M17.029 16.53 19.5 19"
+		/>
+	</svg>
+);
+export default SearchIcon;
