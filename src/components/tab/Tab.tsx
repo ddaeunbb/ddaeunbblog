88a1f5b19ab07df19cf01a51a0b3a3f4b89a0726
ddaeunbb <ddaeunbb@gmail.com>
2023-09-18 22:56:13 +0900

    doc: log추가

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from 'next/link';
 import { ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
 
 interface TabProp {
 	// title: string;
@@ -8,10 +11,14 @@ interface TabProp {
 }
 
 export default function Tab({ path, children }: TabProp) {
+	const pathname = usePathname();
+
 	return (
 		<Link
 			href={path}
-			className="flex items-center px-3 py-1.5 transition-all font-normal rounded-lg hover:bg-[rgba(237,237,237,1)] text-tabGary"
+			className={`flex items-center px-3 py-1.5 transition-all font-normal rounded-lg hover:bg-[#ededed] text-tabGary ${
+				pathname === path && 'font-[600]'
+			}`}
 		>
 			{children}
 		</Link>
